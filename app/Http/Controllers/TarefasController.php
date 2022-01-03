bbf8ad915e237eb12d20984087ad1bc30d86556c
Luís Gustavo <gustavovacodar@gmail.com>
2022-01-03 10:54:49 -0300

    ajuste no banco de dados

--- app/Http/Controllers/TarefasController.php
+++ app/Http/Controllers/TarefasController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TarefasController extends Controller
 {
     public function index(){
-        return view('index');
+        $tarefas = DB::select('select * from tb_tarefas');
+
+        return view('index', ['tarefas'=>$tarefas]);
     }
 }
