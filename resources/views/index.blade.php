43e9430dcbc71b0be06196f10d0f75afd684a368
Luís Gustavo <gustavovacodar@gmail.com>
2022-01-05 07:44:04 -0300

    criação do read

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,48 @@
 
 @section('titulo da aba', 'Lista de Tarefas')
 
-@section('titulo', 'Listagem de Tarefas')
+@section('titulo', 'Listagem de Tarefas') 
+
+@section('conteudo')
+    <h2>alguma coisa</h2>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Status</th>
+                <th scope="col">Tarefa</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if(!empty($tarefas))
+                @foreach($tarefas as $tarefa)
+                    <tr>
+                        <th scope="row">{{$tarefa->id}}</th>
+                        <td>{{$tarefa->status}}</td>
+                        <td>{{$tarefa->tarefa}}</td>
+                        <td>
+                            <a href="">
+                                <button>
+                                    Editar
+                                </button>
+                            </a>
+
+                            <a href="">
+                                <button>
+                                    Apagar
+                                </button>
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
+
+       
+                
+            
+
+@endsection
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -4,11 +4,13 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link >
+        <link rel="stylesheet" href="bootstrap5/css/bootstrap.min.css">
         <title>@yield('titulo da aba')</title>
     </head>
 
     <body>
         <h1>@yield('titulo')</h1>
+
+        @yield('conteudo')
     </body>
 </html>
